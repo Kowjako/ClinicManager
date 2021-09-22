cf0d10a238380eddfc8aae616e87611149f4ab15
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-09-22 20:19:25 +0200

    Complet DataSource and interafces for Articles

--- ClinicManager/Interfaces/IArticleDetailsViewModel.cs
+++ ClinicManager/Interfaces/IArticleDetailsViewModel.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using ClinicManager.DataAccessLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Test.Form1;
 
 namespace ClinicManager.Interfaces
 {
     interface IArticleDetailsViewModel
     {
+        void SaveArticle(Drugs drug, DetailsMode mode);
+        void DeleteArticle(DrugRow drug);
+        BindingSource RefreshArticles();
+        List<DrugRow> Filter();
+        void EditArticle(DrugRow row);
+        void AddArticle();
     }
 }
