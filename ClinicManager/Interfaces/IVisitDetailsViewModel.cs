3455e5399e252e84d72cd777f596c8d6d20df55b
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-09-22 21:05:21 +0200

    Add BindingSource and registration interface

--- ClinicManager/Interfaces/IVisitDetailsViewModel.cs
+++ ClinicManager/Interfaces/IVisitDetailsViewModel.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using ClinicManager.DataAccessLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Test.Form1;
 
 namespace ClinicManager.Interfaces
 {
     interface IVisitDetailsViewModel
     {
+        void SaveVisit(Registrations visit, DetailsMode mode);
+        void DeleteVisit(RegistrationRow visit);
+        BindingSource RefreshVisits();
+        List<RegistrationRow> Filter();
+        void EditRegistration(RegistrationRow row);
+        void AddRegistration();
     }
 }
