e636297f9ae0823a1281f3947e0e11e00c7029f1
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-09-23 20:14:43 +0200

    Add datasources and interfaces for operation details

--- ClinicManager/Interfaces/IOperationDetailsViewModel.cs
+++ ClinicManager/Interfaces/IOperationDetailsViewModel.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using ClinicManager.DataAccessLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Test.Form1;
 
 namespace ClinicManager.Interfaces
 {
     interface IOperationDetailsViewModel
     {
+        void SaveOperation(Operations op, DetailsMode mode);
+        void DeleteOperation(OperationRow op);
+        BindingSource RefreshOperations();
+        List<OperationRow> Filter();
+        void EditOperation(OperationRow row);
+        void AddOperation();
     }
 }
