9ce6e156c471235ca977676ae6cfeaab7491b46f
vladimir <kowyako@yandex.ru>
2021-09-24 15:42:43 +0200

    Add interface and datasources for Costs Details

--- ClinicManager/Interfaces/ICostDetailsViewModel.cs
+++ ClinicManager/Interfaces/ICostDetailsViewModel.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using ClinicManager.DataAccessLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Test.Form1;
 
 namespace ClinicManager.Interfaces
 {
     interface ICostDetailsViewModel
     {
+        void SaveCost(Costs cost, DetailsMode mode);
+        void DeleteCost(CostRow cost);
+        BindingSource RefreshCosts();
+        List<CostRow> Filter();
+        void EditCost(CostRow row);
+        void AddCost();
     }
 }
