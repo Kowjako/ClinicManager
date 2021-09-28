cac9615414f8858764154edcf7491441c204a429
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-09-28 19:20:45 +0200

    Add hierarchy form control

---
+++ ClinicManager/Controls/HierarchyControl.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace ClinicManager.Controls
+{
+    partial class HierarchyControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clinicTree = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // clinicTree
+            // 
+            this.clinicTree.Location = new System.Drawing.Point(12, 12);
+            this.clinicTree.Name = "clinicTree";
+            this.clinicTree.Size = new System.Drawing.Size(272, 444);
+            this.clinicTree.TabIndex = 0;
+            // 
+            // HierarchyControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 468);
+            this.Controls.Add(this.clinicTree);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "HierarchyControl";
+            this.Text = "Struktura praw przychodni";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView clinicTree;
+    }
+}
---
+++ ClinicManager/Controls/HierarchyControl.cs
@@ -0,0 +1,60 @@
+﻿using ClinicManager.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClinicManager.Controls
+{
+    public partial class HierarchyControl : Form
+    {
+        public HierarchyControl()
+        {
+            InitializeComponent();
+        }
+
+        public void SetHierarchy(ClinicRow row)
+        {
+            var mainNode = new TreeNode();
+            mainNode.Name = "workersNode";
+            mainNode.Text = "Pracownicy";
+            clinicTree.Nodes.Add(mainNode);
+
+            var managerNode = new TreeNode();
+            managerNode.Text = "Kierownik";
+
+            var smallEmployee = new TreeNode();
+            smallEmployee.Text = "Młodszy lekarz";
+
+            var employee = new TreeNode();
+            employee.Text = "Lekarz";
+
+            var headEmployee = new TreeNode();
+            headEmployee.Text = "Główny lekarz";
+
+            clinicTree.Nodes[0].Nodes.Add(managerNode);
+            clinicTree.Nodes[0].Nodes.Add(headEmployee);
+            clinicTree.Nodes[0].Nodes.Add(employee);
+            clinicTree.Nodes[0].Nodes.Add(smallEmployee);
+
+            using (var context = new ClinicDataEntities())
+            {
+                var employees = context.Employees.Where(p => p.ClinicId == row.Id);
+                var employeesRow = context.EmployeeRow.AsEnumerable().Where(p => employees.Any(x => x.Id == p.Id));
+                foreach (var emp in employeesRow)
+                {
+                    if (emp.Stanowisko == "Kierownik przychodni") clinicTree.Nodes[0].Nodes[0].Nodes.Add(new TreeNode() { Text = emp.Lekarz });
+                    if (emp.Stanowisko == "Glowny lekarz") clinicTree.Nodes[0].Nodes[1].Nodes.Add(new TreeNode() { Text = emp.Lekarz });
+                    if (emp.Stanowisko == "Lekarz") clinicTree.Nodes[0].Nodes[2].Nodes.Add(new TreeNode() { Text = emp.Lekarz });
+                    if (emp.Stanowisko == "Mlodszy lekarz") clinicTree.Nodes[0].Nodes[3].Nodes.Add(new TreeNode() { Text = emp.Lekarz });
+                }
+            }
+            
+        }
+    }
+}
