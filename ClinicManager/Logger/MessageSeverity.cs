e5720bc2af821e7d8d505baa409652cc75b07f6d
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-12-06 20:22:31 +0100

    Start implementing UILogger

---
+++ ClinicManager/Logger/MessageSeverity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicManager.Logger
+{
+    public enum MessageSeverity
+    {
+        [System.ComponentModel.Description("Informacja: ")]
+        Information = 1,
+        [System.ComponentModel.Description("Ostrzeżenie: ")]
+        Warning = 2,
+        [System.ComponentModel.Description("Błąd: ")]
+        Error = 3,
+        [System.ComponentModel.Description("Zakońcono pomyślnie")]
+        Finish = 4
+    }
+}
